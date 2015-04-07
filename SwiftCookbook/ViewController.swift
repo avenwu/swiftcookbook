2f4b83e95c7f23e7332555ff12a2c71bfa49965f
Chaobin Wu <iamavenwu@gmail.com>
2015-04-07 19:32:52 +0800

    Initial Commit

---
+++ SwiftCookbook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftCookbook
+//
+//  Created by aven wu on 4/7/15.
+//  Copyright (c) 2015 avenwu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
